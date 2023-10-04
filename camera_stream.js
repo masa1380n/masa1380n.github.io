f7273d7eee0b7d3588be1ae234cefce33180d672
masa1380n <mayakashi.n@gmail.com>
2023-10-04 15:45:50 +0900

    select device

--- camera_stream.js
+++ camera_stream.js
@@ -13,3 +13,34 @@ const cameraInit = () => {
             console.log(e);
         });
 }
+
+function populateCameras() {
+    if (!("mediaDevices" in navigator)) return;
+    navigator.mediaDevices.enumerateDevices().then(mediaDevices => {
+        while (cameraSelect.options.length > 0) {
+            cameraSelect.remove(0);
+        }
+        const defaultOption = document.createElement("option");
+        defaultOption.id = "default";
+        defaultOption.textContent = "(default camera) ";
+        cameraSelect.appendChild(defaultOption);
+
+        const videoInputDevices = mediaDevices.filter(
+            mediaDevice => mediaDevice.kind == "videoinput"
+        );
+        if (videoInputDevices.length > 0) {
+            cameraSelect.disabled = false;
+        }
+        videoInputDevices.forEach((videoInputDevice, index) => {
+            if (!videoInputDevice.deviceId) {
+                return;
+            }
+            const option = document.createElement("option");
+
+            option.id = videoInputDevice.deviceId;
+            option.textContent = videoInputDevice.label || `Camera ${index + 1}`;
+            option.selected = deviceId == option.id;
+            cameraSelect.appendChild(option);
+        });
+    });
+}
